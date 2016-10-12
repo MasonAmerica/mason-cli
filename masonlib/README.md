20fb4ffe7a6ccc61584d15572653dbacae30162e
Adnan Begovic <adnan@bymason.com>
2016-10-12 13:30:25 -0700

    mason-cli: Initial Release.
    mason-cli: Introduce preliminary cli for mason.
    
      Create mason publish and mason auth interfaces.
    
    Change-Id: Ifae42d99edb86ca4d1a417f36eb6ba3268709f27
    
    mason-cli: Denote README as markdown.
    
    Change-Id: I9990fc7147fdda0d8219508ebcdb44893f714623
    
    mason-cli: Abstract out mason library.
    
    Change-Id: I9144df9152de56175c48e4d28e28df4f9c792345
    
    mason-cli: Add tests for persist and store classes
    
    Change-Id: If7a0f91c933e82d8380ba324b0de01fddc82d608
    
    mason-cli: Add check for Android Debug key.
    
      Check for `Android Debug` CN in the certificate
      of an apk attempting to be uploaded.
    
    Change-Id: I459da34da2b6e4df8041fb98ccbc2e6f3fcdc359
    
    mason-cli: Make mason python script executable.
    
    Change-Id: I503d41ceda4dd027022c7716e6e481137640ced6
    
    mason-cli: Include apk_parse wholly.
    
    Change-Id: I3371c84751c1b2dc57616ba4febb2edd7ea17ae7
    
    mason-cli: Remove M2Crypto dependency.
    
      This kills certificate parsing until we can find a good
      cross platform soluation.
    
      Also, enumerate all the packages utilized in the cli tool.
    
    Change-Id: I3fd99bc0b1920228f8acc4aeca91d8d679536cf1
    
    mason-cli: Include pyyaml as a requirement.
    
    Change-Id: I3e9d290ea14fe0d05793dd3566d089ae52cc32a9
    
    Update configuration, dynamically create if unavailable
    
    Change-Id: I86fb2fad5c9de5e8c8f5741fc8641f2975090b43
    
    mason-cli: Add progress bar for uploads.
    
    Change-Id: I731af3c29ad6056e00968eb970413e10bc054bf0
    
    mason-cli: Point at production by default.
    
    Change-Id: I1574e46d106e423dec9bd598ff17c4e4580833d0
    
    Updating documentation
    
    Change-Id: If73746c66a20fa0de94eb43f7c596a342c1ec19a
    
    update readme to be more inline with product messaging
    
    Change-Id: I2c09396d9d6b8e5c0119df48f517a8abeefacc5b

---
+++ masonlib/README.md
@@ -0,0 +1,4 @@
+# masonlib
+
+## Testing:
+`python -m unittest discover masonlib/test/`
---
+++ masonlib/test/test_persist.py
@@ -0,0 +1,43 @@
+# COPYRIGHT MASONAMERICA
+import unittest
+import os
+
+from masonlib.persist import Persist
+
+class PersistTest(unittest.TestCase):
+    ID_TOKEN = 'akdfPODIFsplerHSPODif123'
+    ACCESS_TOKEN = 'DSPFOSURELjopISUFpsd8USFDoiu34'
+
+    def setUp(self):
+        self.persist = Persist('.testmasonrc')
+        self.__write_test_tokens()
+        self.persist.reload()
+
+    def tearDown(self):
+        os.remove(self.persist.file)
+
+    def __write_test_tokens(self):
+        test_data = {'id_token': self.ID_TOKEN,
+                     'access_token': self.ACCESS_TOKEN}
+        self.persist.write_tokens(test_data)
+
+    def test_retrieve_id_token(self):
+        assert(self.ID_TOKEN == self.persist.retrieve_id_token())
+
+    def test_retrieve_access_token(self):
+        assert(self.ACCESS_TOKEN == self.persist.retrieve_access_token())
+
+    def test_write_tokens(self):
+        secondary_id_token = 'DSPFOiudapofsd8uaesrljn'
+        secondary_access_token = 'PSDOFislarkewpfdsauaser'
+        secondary_data = {'id_token': secondary_id_token,
+                     'access_token': secondary_access_token}
+        self.persist.write_tokens(secondary_data)
+
+        # reload
+        self.persist.reload()
+        assert(secondary_id_token == self.persist.retrieve_id_token())
+        assert(secondary_access_token == self.persist.retrieve_access_token())
+
+if __name__ == '__main__':
+    unittest.main()
