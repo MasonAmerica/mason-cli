d4a0fe7bb9afd97e24033a5ff3509a7001cb0907
Alex Saveau <alexsaveau@bymason.com>
2019-11-17 16:41:37 -0800

    Add icon to Windows executable
    Signed-off-by: Alex Saveau <alexsaveau@bymason.com>

--- scripts/gen_win_exe.sh
+++ scripts/gen_win_exe.sh
@@ -5,4 +5,5 @@ PYTHONHASHSEED=8 wine ~/.wine/drive_c/Python37/Scripts/pip.exe install .
 PYTHONHASHSEED=8 wine ~/.wine/drive_c/Python37/Scripts/pyinstaller.exe masonlib/mason.py --onefile \
   --add-data "C:\\Python37\\Lib\\site-packages\\pyaxmlparser\\resources\\public.xml;pyaxmlparser\\resources" \
   --add-data "VERSION;." \
-  --add-data "openssl-win;masonlib/internal"
+  --add-data "openssl-win;masonlib/internal" \
+  --icon m.ico
