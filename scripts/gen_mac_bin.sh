e1aa893dd00f1e8ba2b25d0c22b7a18de94b557a
Brint E. Kriebel <gitlab@bekit.net>
2019-12-09 17:29:57 +0000

    gitlab-ci: Create folder based installation for OSX
    The single-file version creates temp files during every execution.
    Catalina scans these files each time the app is executed, which causes a
    5-10 second delay for each command. Distribute the package as .tar.gz
    file of a folder-based pyinstall package for MacOS to avoid this
    slowdown.

--- scripts/gen_mac_bin.sh
+++ scripts/gen_mac_bin.sh
@@ -2,6 +2,6 @@
 
 echo "__version__ = '$(cat VERSION)'" > masonlib/version.py
 pip3 install .
-pyinstaller masonlib/mason.py --onefile \
+pyinstaller masonlib/mason.py \
   --add-data "$(pip3 show pyaxmlparser | grep Location | cut -c11-)/pyaxmlparser/resources/public.xml:pyaxmlparser/resources" \
   --add-data VERSION:.
