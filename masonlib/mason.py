e24f2458df8272f974f5f0c9f2e1f8a9fc9dd9e5
Jonathan Herriott <herriojr@bymason.com>
2020-01-06 12:03:47 -0800

    Default To Turbo
    Now the user can do --no-turbo to disable if needed.
    
    Change-Id: I72d02b9cdbbe08559eac675a001495d8f6801cb1

--- masonlib/mason.py
+++ masonlib/mason.py
@@ -211,7 +211,7 @@ def media(config, type, name, version, media):
 @cli.command()
 @click.option('--await', 'block', is_flag=True, default=False,
               help='Wait synchronously for the build to finish before continuing.')
-@click.option('--turbo', is_flag=True, default=False,
+@click.option('--turbo/--no-turbo', is_flag=True, default=True,
               help='Enable fast Mason config builds (beta).')
 @click.argument('project')
 @click.argument('version', type=click.IntRange(min=0))
@@ -249,7 +249,7 @@ def build(config, block, turbo, project, version):
               help='Don\'t require confirmation.')
 @click.option('--await', 'block', is_flag=True, default=False,
               help='Wait synchronously for the build to finish before continuing.')
-@click.option('--turbo', is_flag=True, default=False,
+@click.option('--turbo/--no-turbo', is_flag=True, default=True,
               help='Enable fast Mason config builds (beta).')
 @click.option('--skip-verify', '-s', is_flag=True, default=False, hidden=True,
               help='Don\'t require confirmation.')
